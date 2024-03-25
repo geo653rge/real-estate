63ab3da69d88fe0a970e404fbe48c5054aedcecd
geo653rge <georgekimanikiringa@gmail.com>
2024-03-25 12:17:19 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <h1 className="text-bold text-9xl text-purple-600">
+    <h1 className="font-bold  text-5xl text-purple-600">
       
       Welcome George to this project that you are about to undertake
     </h1>
